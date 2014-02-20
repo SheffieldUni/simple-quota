ed70264c30d64e4f20af5519703863b88b541ee6
Tim Parkinson <t.r.parkinson@sheffield.ac.uk>
2014-02-20 11:37:53 +0000

    Added some user path resolution functions

---
+++ User.Tests.ps1
@@ -0,0 +1,89 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
+. "$here\$sut"
+
+if (-not (Get-Command -Name get-aduser -ErrorAction SilentlyContinue)) {function get-aduser {}}
+
+Describe "Get-UserHomeDirectory" {
+    $real_user = 'user1'
+    $broken_user = 'user2'
+    $home = '\\server\home\user1'
+    $profilepath = '\\server\profile\user1'
+
+    Mock Get-ADUser {
+        param($identity)
+
+        if ($identity -eq $real_user) {
+            @{
+                DistinguishedName = 'CN=User1,OU=blah,DC=something,DC=com'
+                Enabled = $true
+                GivenName = 'Real'
+                Name = 'User1'
+                ObjectClass = 'user'
+                ObjectGuid = 'jhagsjhgdjhsdgajhsgdjhsgadjasghdghdjashsdgajh'
+                HomeDirectory = $home
+                SID = 'S-1-1-11-11111111-11111111'
+                Surname = 'User'
+                UserPrincipalName = 'user1@something.com'
+            }
+        } else {
+            Write-Error "No user found" -ErrorAction Stop
+        }
+    }
+
+    Context "when called on an existing user" {
+    
+        $result = Get-UserHomeDirectory -Username $real_user
+        It "should return the homedirectory attribute" {
+            $result | Should Be $home
+        }
+    }
+
+    Context "when called on a non-existent user" {
+        It "should throw an error" {
+            {Get-UserHomeDirectory -Username $broken_user} | Should throw
+        }
+    }
+}
+
+Describe "Get-UserProfileDirectory" {
+    $real_user = 'user1'
+    $broken_user = 'user2'
+    $home = '\\server\home\user1'
+    $profilepath = '\\server\profile\user1'
+
+        Mock Get-ADUser {
+            param($identity)
+
+            if ($identity -eq $real_user) {
+                @{
+                    DistinguishedName = 'CN=User1,OU=blah,DC=something,DC=com'
+                    Enabled = $true
+                    GivenName = 'Real'
+                    Name = 'User1'
+                    ObjectClass = 'user'
+                    ObjectGuid = 'jhagsjhgdjhsdgajhsgdjhsgadjasghdghdjashsdgajh'
+                    ProfilePath = $profilepath
+                    SID = 'S-1-1-11-11111111-11111111'
+                    Surname = 'User'
+                    UserPrincipalName = 'user1@something.com'
+                }
+            } else {
+                Write-Error "No user found" -ErrorAction Stop
+            }
+        }
+
+    Context "when called on an existing user" {
+        
+        $result = Get-UserProfileDirectory -Username $real_user
+        It "should return the profile attribute" {
+            $result | Should Be $profilepath
+        }
+    }
+    
+    Context "when called on a non-existent user" {
+        It "should throw an error" {
+            {Get-UserProfileDirectory -Username $broken_user} | Should throw
+        }
+    }
+}
---
+++ User.ps1
@@ -0,0 +1,51 @@
+﻿# simple-quota user functions
+function Get-UserHomeDirectory {
+    [CmdletBinding()]
+
+    param (
+        [Parameter(Mandatory=$true)]
+        $Username
+    )
+
+    begin {}
+
+    process {
+        try {
+            $user = Get-Aduser -Identity $Username -Property Homedirectory 
+        }
+        catch 
+        {
+            Write-Error "Could not find user" -ErrorAction Stop
+        }
+
+        $user.HomeDirectory
+    }
+
+    end {}
+}
+
+function Get-UserProfileDirectory {
+
+    [CmdletBinding()]
+
+    param (
+        [Parameter(Mandatory=$true)]
+        $Username
+    )
+
+    begin {}
+
+    process {
+        try {
+            $user = Get-Aduser -Identity $Username -Property ProfilePath 
+        }
+        catch 
+        {
+            Write-Error "Could not find user" -ErrorAction Stop
+        }
+
+        $user.Profilepath
+    }
+
+    end {}
+}
