b7799e2eb6bd490ac86cd56caafee201637bdd12
Tim Parkinson <t.r.parkinson@sheffield.ac.uk>
2014-03-17 14:47:50 +0000

    Added ps1 import in module file

--- simple-quota.psm1
+++ simple-quota.psm1
@@ -1,1 +1,5 @@
-﻿
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+Get-ChildItem -Path "$here\*" -Include "*.ps1" -Exclude "*.Tests.ps1" |
+    ForEach-Object {
+        . $_.FullName
+    }
